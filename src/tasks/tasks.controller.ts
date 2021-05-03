9301aa3a610fbc6bd8af1dba0e80ac43723afa97
abhina-22 <abhinasathyan1@gmail.com>
2021-05-03 10:26:21 +0530

    added data persistence

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,40 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { query } from 'express';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { Task } from './tasks.entity';
+import { TaskStatus } from './task.status.enum';
 
 
 @Controller('tasks')
 export class TasksController {
     constructor( private tasksService: TasksService){}
 
-    @Post()
-    createTask(@Body('title') title: string, @Body('description') description: string): Task{
-        return  this.tasksService.createTask(title, description)
-    }
-
     @Get()
-    getAllTasks() :Task[]{
-        return this.tasksService.getAllTasks()
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) :Promise <Task []>{
+        return this.tasksService.getTasks(filterDto)
     }
 
-    
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(@Body() createTaskDto: CreateTaskDto): Promise <Task>{
+        return  this.tasksService.createTask(createTaskDto)
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task>{
+        return this.tasksService.getTaskById(id)
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void>{
+        return this.tasksService.deleteTask(id)
+    }
+
+    @Patch('/:id/status')
+    updateTask(@Param('id', ParseIntPipe) id: number, @Body('status', TaskStatusValidationPipe) status: TaskStatus): Promise<Task>{
+        return this.tasksService.updateTask(id, status)
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,20 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { v1 as uuid} from 'uuid';
-import { Task, TaskStatus } from './tasks.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { Task } from './tasks.entity';
+import { TaskStatus } from './task.status.enum';
 
 @Injectable()
 export class TasksService {
-    private tasks:Task[] = []
 
-    getAllTasks(): Task[]{
-        return this.tasks
+    constructor(
+        @InjectRepository(TaskRepository)
+        private taskRespository :TaskRepository,
+    ){}
+
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]>{
+        return this.taskRespository.getTasks(filterDto)
+    }
+    
+
+    async getTaskById(id: number): Promise<Task>{
+        const found =  await this.taskRespository.findOne(id)
+        if (!found){
+            throw new NotFoundException("Task with id not found")
+        }
+        return found
     }
 
-    createTask(title: string, description: string): Task{
-        const task: Task ={
-            title, description, status: TaskStatus.OPEN, id: uuid()
+    async createTask(createTaskDto: CreateTaskDto): Promise <Task>{
+       return this.taskRespository.createTask(createTaskDto)
+    }
+
+    async deleteTask(id:number): Promise<void>{
+        const result =  await this.taskRespository.delete(id)
+        if (!result.affected){
+            throw new NotFoundException("Task with id not found")
         }
-        this.tasks.push(task)
-        return task
+    }
+
+    async updateTask(id: number, status: TaskStatus): Promise<Task>{
+        let taskToUpdate = await this.getTaskById(id)
+        taskToUpdate.status = status
+        await taskToUpdate.save()
+        return taskToUpdate
     }
 }
